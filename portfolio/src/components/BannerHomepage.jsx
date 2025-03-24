c8c0bdfb1775f0abe659137557e1d7ddae999ea6
Sandra <sandra.rossi17@hotmail.com>
2025-03-24 22:12:05 +0100

    homepage quasi fini + debut section à propos

--- portfolio/src/components/BannerHomepage.jsx
+++ portfolio/src/components/BannerHomepage.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ordinateur from '../assets/ordi.png';
 
+const words = ["réalité", "succès", "projets"];
 
 const BannerHomepage = () => {
+
+    const [wordChange, setWordChange] = useState(0);
+    const [typedWord, setTypedWord] = useState('');
+    const [isTyping, setIsTyping] = useState(true);
+
+    useEffect(() => {
+        let typingInterval;
+        if (isTyping) {
+            // Si on tape un mot
+            typingInterval = setInterval(() => {
+                setTypedWord((prevWord) => {
+                    const wordToType = words[wordChange] + '.';
+                    const newWord = wordToType.slice(0, prevWord.length + 1);
+                    return newWord;
+                });
+            }, 200); // Vitesse de "tapage" (100ms par caractère)
+        } else {
+            // On a terminé de taper, on change de mot après un délai
+            const nextWordIndex = (wordChange + 1) % words.length;
+            setTimeout(() => {
+                setTypedWord(''); // Efface le mot en cours
+                setIsTyping(true); // Reprend l'écriture du mot suivant
+                setWordChange(nextWordIndex); // Passe au mot suivant
+            }, 2000); // Délai avant de commencer à taper le prochain mot
+        }
+
+        // Quand le mot est complètement tapé, on arrête l'intervalle
+        if (typedWord.length === words[wordChange].length) {
+            clearInterval(typingInterval);
+            setIsTyping(false); // On arrête de taper
+        }
+
+        // Nettoyage de l'intervalle à chaque mise à jour
+        return () => clearInterval(typingInterval);
+    }, [typedWord, isTyping, wordChange]);
+
     return (
         <div className='banner-container'>
             <section className="overlay">
@@ -10,7 +47,8 @@ const BannerHomepage = () => {
                 <p>
                     Votre vision, mon code : <br />
                     ensemble, transformons vos <br />
-                    idées en réalité.
+                    idées en
+                    <span className="word-change">  {typedWord}</span>
                 </p>
             </section>
             <div className="image-container">
