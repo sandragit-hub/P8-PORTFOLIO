a30476f5422d27044bbde35e648ba9a1c4b58a05
Sandra <sandra.rossi17@hotmail.com>
2025-03-31 14:28:43 +0200

    ajout routes des projets

--- backend/controleur/project.js
+++ backend/controleur/project.js
@@ -1,11 +1,35 @@
 const Project = require('.models/Projects');
 
+
+// Contrôleur pour ajouter les compétences 
+
+exports.createProject = async (req, res, next) => {
+    try {
+        // Créer un nouveau projet avec les données envoyées dans la requête
+        const { title, image } = req.body;
+        const newProject = new Project({
+            title,
+            image,
+        });
+
+        // Sauvegarder la compétence dans la base de données
+        await newSProject.save();
+
+        // Répondre avec la compétence créée
+        res.status(201).json(newProject);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+
+
 // Controleur pour recuperer les projets
 
 
 exports.getProjects = async (req, res, next) => {
     try {
-        const projects = await Skill.find();
+        const projects = await Project.find();
         res.status(200).json(projects);
     } catch (error) {
         res.status(400).json({ error });
