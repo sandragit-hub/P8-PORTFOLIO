46b6db5935eede07770dd82f5f30af5491ae1be8
Sandra <sandra.rossi17@hotmail.com>
2025-05-08 09:45:29 +0200

    modif tps de chargement

--- portfolio/src/components/Loader.jsx
+++ portfolio/src/components/Loader.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-const Loader = () => {
+function Loader({ onFinish }) {
+    const [progress, setProgress] = useState(0);
+    const gifUrl = '/assets/background.gif';
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = gifUrl;
+
+        let interval; // Déclaration de l'intervalle ici, en dehors du `useEffect`
+
+        img.onload = () => {
+            // Quand le gif est chargé, on démarre la progression de la barre
+            interval = setInterval(() => {
+                setProgress((prev) => {
+                    if (prev >= 100) {
+                        clearInterval(interval);
+                        setTimeout(onFinish, 300); // Petite pause à la fin
+                        return 100;
+                    }
+                    return prev + 2;
+                });
+            }, 20);
+        };
+
+        return () => {
+            // Nettoyer l'intervalle même si le composant est démonté
+            if (interval) {
+                clearInterval(interval);
+            }
+        };
+    }, [onFinish, gifUrl]); // Dépendances : on relance l'effet si `gifUrl` change
+
     return (
         <div className="loader-container">
             <div className="loading-bar">
-                <div className="progress"></div>
+                <div
+                    className="progress"
+                    style={{ width: `${progress}%` }}
+                ></div>
             </div>
         </div>
     );
-};
+}
 
 export default Loader;
