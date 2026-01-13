161dcec95cdb2903a915be5a3a5f167d5104dbe3
Sandra <sandra.rossi17@hotmail.com>
2026-01-13 08:32:22 +0100

    video en background pour alléger le site

--- portfolio/src/components/Loader.jsx
+++ portfolio/src/components/Loader.jsx
@@ -2,32 +2,27 @@ import { useEffect, useState } from "react";
 
 function Loader({ onFinish }) {
   const [progress, setProgress] = useState(0);
-  const videoUrl = "/assets/background.mp4"; // ta nouvelle vidéo
 
   useEffect(() => {
-    const video = document.createElement("video");
-    video.src = videoUrl;
-
     let interval;
 
     // Quand la vidéo est chargée (metadata / data)
-    video.onloadeddata = () => {
-      interval = setInterval(() => {
-        setProgress((prev) => {
-          if (prev >= 100) {
-            clearInterval(interval);
-            setTimeout(onFinish, 300);
-            return 100;
-          }
-          return prev + 2;
-        });
-      }, 20);
-    };
+
+    interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          setTimeout(onFinish, 300);
+          return 100;
+        }
+        return prev + 2;
+      });
+    }, 20);
 
     return () => {
       if (interval) clearInterval(interval);
     };
-  }, [onFinish, videoUrl]);
+  }, [onFinish]);
 
   return (
     <div className="loader-container">
